bc1479fd471f63686e3ca8ecdbe31a7db8b23b33
Paul Booth <thepaulbooth@gmail.com>
2011-04-06 18:44:55 -0400

    Hello world! We have a video encoder, with hello world printing out.

--- include/system.h
+++ include/system.h
@@ -8,5 +8,10 @@ extern unsigned short *memsetw(unsigned short *dest, unsigned short val, int cou
 extern int strlen(const char *str);
 extern unsigned char inportb (unsigned short _port);
 extern void outportb (unsigned short _port, unsigned char _data);
+extern void cls();
+extern void putch(unsigned char c);
+extern void puts(unsigned char *str);
+extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
+extern void init_video();
 
 #endif
--- src/kernel.c
+++ src/kernel.c
@@ -93,15 +93,8 @@ void kmain( void* mbd, unsigned int magic )
 	const int SCREEN_WIDTH = 80;
 	const int SCREEN_HEIGHT = 25;
 	
-	
-	int max_x = strlen(test_string);
-	int x, y;
-	for (y = 0; y < SCREEN_HEIGHT; y++) {
-		for (x = 0; x < max_x; x++) {
-			videoram[y*2*SCREEN_WIDTH + x*2] = char_A;	/* character 'A' */
-			videoram[y*2*SCREEN_WIDTH + x*2+1] = 0x07;	/* forground, background color. */
-		}
-	}
+	init_video();
+	puts("Hello OSWORLD!!!");
 
 	/* Write your kernel here. */
 }
