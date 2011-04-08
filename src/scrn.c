13ab65bf223204a0bc8f032d6549ffe886093e35
Tim Cameron Ryan <dev@tim-ryan.com>
2011-04-08 14:51:49 -0400

    Moves Jialiya's text colors to the appropos place

--- src/scrn.c
+++ src/scrn.c
@@ -1,5 +1,22 @@
 #include <system.h>
 
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHT_GREY 7
+#define DARK_GREY 8
+#define LIGHT_BLUE 9
+#define LIGHT_GREEN 10
+#define LIGHT_CYAN 11
+#define LIGHT_RED 12
+#define LIGHT_MAGENTA 13
+#define LIGHT_BROWN 14
+#define WHITE 15
+
 /* These define our textpointer, our background and foreground
 *  colors (attributes), and x and y cursor coordinates */
 unsigned short *textmemptr;
