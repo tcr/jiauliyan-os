97011bab61398d4cdf161fb7f764edd4d8b9ab6d
Tim Cameron Ryan <dev@tim-ryan.com>
2011-04-08 14:52:56 -0400

    Moves text color defines around

--- include/system.h
+++ include/system.h
@@ -1,17 +1,36 @@
 #ifndef __SYSTEM_H
 #define __SYSTEM_H
 
-/* MAIN.C */
+/* kernel.c */
 extern unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
 extern unsigned char *memset(unsigned char *dest, unsigned char val, int count);
 extern unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
 extern int strlen(const char *str);
 extern unsigned char inportb (unsigned short _port);
 extern void outportb (unsigned short _port, unsigned char _data);
+
+/* scrn.c */
 extern void cls();
 extern void putch(unsigned char c);
 extern void puts(unsigned char *str);
 extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
 extern void init_video();
+
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHT_GREY 7
+#define DARK_GREY 8
+#define LIGHT_BLUE 9
+#define LIGHT_GREEN 10
+#define LIGHT_CYAN 11
+#define LIGHT_RED 12
+#define LIGHT_MAGENTA 13
+#define LIGHT_BROWN 14
+#define WHITE 15
 
 #endif
