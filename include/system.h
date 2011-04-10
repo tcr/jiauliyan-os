ecf44d404600a6f3da74c9678fe111acd80dd25f
Jialiya Huang <jialiya.huang@students.olin.edu>
2011-04-10 15:07:03 -0400

    Added IDT

--- include/system.h
+++ include/system.h
@@ -38,4 +38,8 @@ extern void init_video();
 extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
 extern void gdt_install();
 
+/* idt.c */
+extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+extern void idt_install();
+
 #endif
--- src/kernel.c
+++ src/kernel.c
@@ -79,7 +79,8 @@ void kmain( void* mbd, unsigned int magic )
 	}
 
 	gdt_install();
-
+	idt_install();
+	
 	/* You could either use multiboot.h */
 	/* (http://www.gnu.org/software/grub/manual/multiboot/multiboot.html#multiboot_002eh) */
 	/* or do your offsets yourself. The following is merely an example. */ 
