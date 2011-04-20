1b8c13a8a0acdbe71291acfd91d9fe9f3ee1af28
Jialiya Huang <jialiya.huang@students.olin.edu>
2011-04-19 20:19:58 -0400

    string.c

--- src/string.c
+++ src/string.c
@@ -90,9 +90,15 @@ int strncmp(const char *str1, const char *str2, size_t n)
 	//[TODO]
 }
 
-size_t strxfrm(char *str1, const char *str2, size_t n)
+size_t strxfrm(char *s1, const char *s2, size_t n)
 {
-	//[TODO]
+	size_t len = strlen(s2) + 1;
+
+	if (len < n)
+			n = len;
+	memcpy(s1, s2, n);
+	return len;
+
 }
 
 
@@ -100,44 +106,183 @@ size_t strxfrm(char *str1, const char *str2, size_t n)
  * searching
  */
 
-void *memchr(const void *str, int c, size_t n)
+void *memchr(const void *s, int c, size_t n)
 {
-	//[TODO]
+	const unsigned char*  p   = s;
+    const unsigned char*  end = p + n;
+
+    for (;;) {
+        if (p >= end || p[0] == c) break; p++;
+        if (p >= end || p[0] == c) break; p++;
+        if (p >= end || p[0] == c) break; p++;
+        if (p >= end || p[0] == c) break; p++;
+    }
+    if (p >= end)
+        return NULL;
+    else
+        return (void*) p;
+
 }
 
-char *strchr(const char *str, int c)
+char *strchr(const char *p, int ch)
 {
-	//[TODO]
+	 for (;; ++p) {
+			if (*p == ch)
+					return((char *)p);
+			if (!*p)
+					return((char *)NULL);
+	}
+
 }
 
-size_t strcspn(const char *str1, const char *str2)
+size_t strcspn(const char *s1, const char *s2)
 {
-	//[TODO]
+	const char *p, *spanp;
+	char c, sc;
+
+	/*
+	 * Stop as soon as we find any character from s2.  Note that there
+	 * must be a NUL in s2; it suffices to stop when we find that, too.
+	 */
+	for (p = s1;;) {
+			c = *p++;
+			spanp = s2;
+			do {
+					if ((sc = *spanp++) == c)
+							return (p - 1 - s1);
+			} while (sc != 0);
+	}
+
 }
 
-char *strpbrk(const char *str1, const char *str2)
+char *strpbrk(const char *s1, const char *s2)
 {
-	//[TODO]
+	const char *scanp;
+	int c, sc;
+
+	while ((c = *s1++) != 0) {
+			for (scanp = s2; (sc = *scanp++) != 0;)
+					if (sc == c)
+							return ((char *)(s1 - 1));
+	}
+	return (NULL);
+
 }
 
-char *strrchr(const char *str, int c)
+char *strrchr(const char *p, int ch)
 {
-	//[TODO]
+	char *save;
+
+	for (save = NULL;; ++p) {
+		if (*p == ch)
+			save = (char *)p;
+		if (!*p)
+			return(save);
+	}
+
 }
 
-size_t strspn(const char *str1, const char *str2)
+size_t strspn(const char *s1, const char *s2)
 {
-	//[TODO]
+	const char *p = s1, *spanp;
+    char c, sc;
+
+    /*
+     * Skip any characters in s2, excluding the terminating \0.
+     */
+	cont:
+		c = *p++;
+		for (spanp = s2; (sc = *spanp++) != 0;)
+			if (sc == c)
+				goto cont;
+    return (p - 1 - s1);
+
 }
 
-char *strstr(const char *str1, const char *str2)
+char *strstr(const char *s1, const char *s2)
 {
-	//[TODO]
+	int l1 = strlen(s1);
+    int l2 = strlen(s2);
+
+    while (l1 >= l2) {
+        if (strcmp(s1, s2) != 0) {
+            return s1;
+        }
+        s1++;
+        l1--;
+    }
+    return 0;
+
 }
 
-char *strtok(char *str1, const char *str2)
+char * strtok_r(char *s, const char *delim, char **last)
 {
-	//[TODO]
+    char *spanp;
+    int c, sc;
+    char *tok;
+
+    if (s == NULL && (s = *last) == NULL)
+    {
+        return NULL;
+    }
+
+    /*
+     * Skip (span) leading delimiters (s += strspn(s, delim), sort of).
+     */
+cont:
+    c = *s++;
+    for (spanp = (char *)delim; (sc = *spanp++) != 0; )
+    {
+        if (c == sc)
+        {
+            goto cont;
+        }
+    }
+
+    if (c == 0)         /* no non-delimiter characters */
+    {
+        *last = NULL;
+        return NULL;
+    }
+    tok = s - 1;
+
+    /*
+     * Scan token (scan for delimiters: s += strcspn(s, delim), sort of).
+     * Note that delim must have one NUL; we stop if we see that, too.
+     */
+    for (;;)
+    {
+        c = *s++;
+        spanp = (char *)delim;
+        do
+        {
+            if ((sc = *spanp++) == c)
+            {
+                if (c == 0)
+                {
+                    s = NULL;
+                }
+                else
+                {
+                    char *w = s - 1;
+                    *w = '\0';
+                }
+                *last = s;
+                return tok;
+            }
+        }
+        while (sc != 0);
+    }
+    /* NOTREACHED */
+}
+
+
+
+char *strtok(char *s, const char *delim)
+{
+	static char *last;
+
+    return strtok_r(s, delim, &last);
 }
 
 
@@ -172,7 +317,7 @@ void *memsetw(void *dest, short int val, int count)
 
 char *strerror(int errnum)
 {
-	//[TODO]
+	return "Error";
 }
 
 int strlen(const char *str)
