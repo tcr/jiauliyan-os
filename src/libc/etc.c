44e05498bfec9f1d31c5a20bf7dfdb5b35ea253d
Paul Booth <thepaulbooth@gmail.com>
2011-04-22 14:36:34 -0400

    added strtod

--- src/libc/etc.c
+++ src/libc/etc.c
@@ -12,6 +12,7 @@
 #include <errno.h>
 #include <limits.h>
 #include <ctype.h>
+#include <math.h>
 
 sighandler_t signal(int signum, sighandler_t handler)
 {
@@ -40,10 +41,118 @@ int system(const char *string)
 	return 1;
 }
 
-double strtod(const char *start, char **endptr)
+double strtod(const char *str, char **endptr)
 {
-	stream_puts(serialstream, "strtod() called.\n");
-	return 0;
+	double number;
+	int exponent;
+	int negative;
+	char *p = (char *) str;
+	double p10;
+	int n;
+	int num_digits;
+	int num_decimals;
+
+	// Skip leading whitespace
+	while (isspace(*p)) p++;
+
+	// Handle optional sign
+	negative = 0;
+	switch (*p) 
+	{             
+	case '-': negative = 1; // Fall through to increment position
+	case '+': p++;
+	}
+
+	number = 0.;
+	exponent = 0;
+	num_digits = 0;
+	num_decimals = 0;
+
+	// Process string of digits
+	while (isdigit(*p))
+	{
+		number = number * 10. + (*p - '0');
+		p++;
+		num_digits++;
+	}
+
+	// Process decimal part
+	if (*p == '.') 
+	{
+		p++;
+
+		while (isdigit(*p))
+		{
+			number = number * 10. + (*p - '0');
+			p++;
+			num_digits++;
+			num_decimals++;
+		}
+
+		exponent -= num_decimals;
+	}
+
+	if (num_digits == 0)
+	{
+		errno = ERANGE;
+		return 0.0;
+	}
+
+	// Correct for sign
+	if (negative) number = -number;
+
+	// Process an exponent string
+	if (*p == 'e' || *p == 'E') 
+	{
+		// Handle optional sign
+		negative = 0;
+		switch(*++p) 
+		{   
+		case '-': negative = 1;   // Fall through to increment pos
+		case '+': p++;
+		}
+
+		// Process string of digits
+		n = 0;
+		while (isdigit(*p)) 
+		{   
+			n = n * 10 + (*p - '0');
+			p++;
+		}
+
+		if (negative) 
+			exponent -= n;
+		else
+			exponent += n;
+	}
+
+	if (exponent < DBL_MIN_EXP  || exponent > DBL_MAX_EXP)
+	{
+		errno = ERANGE;
+		return HUGE_VAL;
+	}
+
+	// Scale the result
+	p10 = 10.;
+	n = exponent;
+	if (n < 0) n = -n;
+	while (n) 
+	{
+		if (n & 1) 
+		{
+			if (exponent < 0)
+				number /= p10;
+			else
+				number *= p10;
+		}
+		n >>= 1;
+		p10 *= p10;
+	}
+
+	if (number == HUGE_VAL) errno = ERANGE;
+	if (endptr) *endptr = p;
+
+	return number;
 }
 
 unsigned long strtoul(char *nptr, char **endptr, int base)
--- src/libc/limits.h
+++ src/libc/limits.h
@@ -2,6 +2,8 @@
 #define UCHAR_MAX 255
 #define SHRT_MAX 32767
 #define ULONG_MAX 0xFFFFFFFFUL
+#define DBL_MIN_EXP (-999)
+#define DBL_MAX_EXP 999
 
 #define RAND_MAX 0x7FFF
 
