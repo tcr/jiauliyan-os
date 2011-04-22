8c4bf6d2489af34ac6ef93efcfcac9928c9b41d6
Jialiya Huang <jialiya.huang@students.olin.edu>
2011-04-22 14:26:25 -0400

    changing lua script

--- src/scripts/os.lua
+++ src/scripts/os.lua
@@ -1,4 +1,9 @@
-print("Hello world!");
+print("Hello world!")
 function foo()
-	print("Hi I am the foo function!");
+	print("Hi I am the foo function!")
+	test = "haha"
+	print(test)
+	--[[for i=0, 10, 1 do
+		print(i)
+	end]]
 end
